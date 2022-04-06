d252e5ff8477ba06951319bfdc80276cf17c986d
Andrew Wong <andrew.wong@masimo.com>
2022-04-05 17:30:56 -0700

    Custom content provider (not working yet)

--- ContentProviderApplication/app/src/main/java/com/fourmainandroidcomponents/contentproviderapplication/MainActivity.kt
+++ ContentProviderApplication/app/src/main/java/com/fourmainandroidcomponents/contentproviderapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fourmainandroidcomponents.contentproviderapplication
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract
 import androidx.appcompat.app.AppCompatActivity
@@ -39,6 +40,34 @@ class MainActivity : AppCompatActivity() {
                         .toList())
 
                     ContactsDialogFragment.newInstance(list).show(supportFragmentManager, ContactsDialogFragment.TAG)
+
+                    close()
+                }
+
+            }
+
+            getCustom.setOnClickListener {
+                val cursor = contentResolver.query(
+                    Uri.parse("content://com.fourmainandroidcomponents.MyContentProvider.AUTHORITY/xxx" ),
+                    arrayOf(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY, ContactsContract.Contacts.HAS_PHONE_NUMBER),
+                    null,
+                    null,
+                    "${ContactsContract.Contacts.DISPLAY_NAME_PRIMARY} ASC"
+                )
+
+                cursor?.apply {
+//                    val nameIndex: Int = getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY)
+//                    val hasPhoneNumberIndex: Int = getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)
+//
+//                    val list = ArrayList(generateSequence { if (moveToNext()) this else null }
+//                        .map {
+//                            val name = getString(nameIndex)
+//                            val hasPhoneNumber = getString(hasPhoneNumberIndex)
+//                            "name: $name, hasPhoneNumber: $hasPhoneNumber"
+//                        }
+//                        .toList())
+//
+//                    ContactsDialogFragment.newInstance(list).show(supportFragmentManager, ContactsDialogFragment.TAG)
 
                     close()
                 }
